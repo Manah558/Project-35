76b6ccd72faa43e90110c924df5aa9eb41cc6dac
Manah558 <77605239+Manah558@users.noreply.github.com>
2021-05-12 12:21:29 +0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,70 +1,75 @@
-var balloon;
-var database, balloonposition;
-
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+var database, position;
 function preload(){
-  database = firebase.database();
-backgroundImg = loadImage("Hot Air Ballon-01.png");
-hotairballoon = loadAnimation("Hot Air Ballon-02.png", "Hot Air Ballon-03.png", "Hot Air Ballon-04.png")
-balloonImg1 = loadImage("Hot Air Ballon-02.png");
-balloonImg2 = loadImage("Hot Air Ballon-03.png");
-balloonImg3 = loadImage("Hot Air Ballon-04.png");
-var ballposition = database.ref("Balloon/Position");
-ballposition.on("value", readPosition, showError);
-}
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
 
+//Function to set initial environment
 function setup() {
-  createCanvas(1000,641);
-  balloon = createSprite(400, 300, 50, 50);
-  balloon.addAnimation("flying",hotairballoon);
-  balloon.scale = 0.7;
+  database=firebase.database();
+  console.log(database);
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  var balloonPosition =database.ref("balloon/height");
+  balloonPosition.on("value", readHeight, showError);
+  textSize(20); 
 }
 
+// function to display UI
 function draw() {
-  background(backgroundImg); 
-  strokeWeight(2);
-  stroke("lightgreen");
-  fill("blue");
-  textSize(20);
-  text("Use the 4 arrow keys to move the Hot Air Balloon", 30, 30);
+  background(bg);
 
   if(keyDown(LEFT_ARROW)){
-  writePosition(-10, 0);
-  balloon.addAnimation("hotairballoon", balloonImg1);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+    balloon.velocityX=-5;
   }
-
-  if(keyDown(RIGHT_ARROW)){
-    writePosition(10, 0);
-    balloon.addAnimation("hotairballoon", balloonImg2);                                                              
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+    balloon.velocityX= 5;
   }
-
-  if(keyDown(UP_ARROW)){
-    writePosition(0, -10);
-    balloon.addAnimation("hotairballoon", balloonImg3); 
-    balloon.scale = balloon.scale-0.02;                                                       
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+    updateHeight(0, -10);
+    balloon.scale=balloon.scale-0.001;
   }
-
-  if(keyDown(DOWN_ARROW)){
-    writePosition(0, 10);
-    balloon.addAnimation("hotairballoon", balloonImg1);     
-    balloon.scale = balloon.scale+0.02;                                                           
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+    updateHeight(0, 10);
+    balloon.velocityY=5;
   }
 
   drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+function updateHeight(x,y){
+  database.ref("balloon/height").update({
+    x:height.x+x,
+    y:height.y+y,
+  })
+}
+function readHeight(){
+  height = data.val();
+  balloon.x=height.x;
+  balloon.y=height.y;
 }
 
-function readPosition(data){
-position = data.val();
-balloon.x = position.x;
-balloon.y = position.y;
-}
-
-function writePosition(x, y){
-database.ref('Balloon/Position').set({
-  'x': balloon.x + x,
-  'y': balloon.y + y,
-})
-}
 
 function showError(){
-  console.log("error");
+  console.log("Error in writing to the database :(");
 }

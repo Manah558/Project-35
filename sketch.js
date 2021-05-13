c4c90c2f0d2572f6c9d549898842451f1579879a
Manah558 <77605239+Manah558@users.noreply.github.com>
2021-05-13 20:46:52 +0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,76 +1,67 @@
-var balloon;
-var BackgroundImg;
-var balloon,balloonImg1,balloonImg2,balloonImg3;
-var database;
-var position;
+var balloon,balloonImg;
+var background,backgroundImg;
 
-function preload(){
-  BackgroundImg = loadImage("images/Hot Air Ballon-01.png");
-  balloonImg= loadAnimation("images/Hot Air Ballon-02.png", "images/Hot Air Ballon-03.png", "images/Hot Air Ballon-04.png");
- 
+function preload() {
+  balloonImg = loadImage("hotairballoon1.png");
+  backgroundImg = loadImage("cityImage.png");
+  
+
 }
 
 function setup() {
-  createCanvas(800,700);
+  createCanvas(800,400);
 
-  database= firebase.database();
-  console.log(database);
-
- balloon= createSprite(250, 300, 50, 50);
- balloon.addAnimation("Hot Air Ballon-02.png",balloonImg); 
- balloon.scale=0.5;
-
- var balloonPosition = database.ref('balloon/position');
- balloonPosition.on("value",readPosition,showError)
+  balloon = createSprite(250,100,20,20);
+  balloon.addImage(balloonImg,"Balloon");
+  balloon.scale = 0.3;
+  
 }
 
-
 function draw() {
-  background(BackgroundImg);
+  background(backgroundImg,"Background"); 
 
+  fill("black");
+  text("Use Arrow Keys to move the Balloon!",10,30);
 
- if(keyDown(LEFT_ARROW)){
-  updatePosition(-10,0)
-    balloon.x=balloon.x-10
-   
-  }
- 
-  if(keyDown(RIGHT_ARROW)){
-    updatePosition(+10,0)
-    balloon.x=balloon.x+10
-   
-  }
- 
-  if(keyDown(UP_ARROW)){
-    updatePosition(0,-10)
-    balloon.y=balloon.y-10
-    balloon.scale=balloon.scale-0.01
-  }
- 
-  if(keyDown(DOWN_ARROW)){
-    updatePosition(0,+10)
-    balloon.y=balloon.y+10
-  
-    balloon.scale=balloon.scale+0.01
+  if(keyDown(LEFT_ARROW)){
+    balloon.x = balloon.x - 10;
+    }
+else if(keyDown(RIGHT_ARROW)){
+    balloon.x = balloon.x + 10;
+}
+else if(keyDown(UP_ARROW)){
+    balloon.y = balloon.y - 10;
+    balloon.scale = 0.35;
+}
+else if(keyDown(DOWN_ARROW)){
+    balloon.y = balloon.y + 10;
+    balloon.scale = 0.25;
   }
 
   drawSprites();
 }
 
-function updatePosition(x,y){
-database.ref('balloon/position').set({
-  'x': balloon.x + x,
-  'y': balloon.y + y
-})
+function changePosition(x,y){
+  ball.x = ball.x + x;
+  ball.y = ball.y + y;
+}
+
+function readValue(data) {
+  position = data.val();
+
+  ball.x = position.x;
+  ball.y = position.y;
 
 }
 
-function readPosition(data){
-  position=data.val();
-  balloon.x=position.x;
-  balloon.y=position.y;
+function showError() {
+  console.log("Error in connecting to Database");
 }
 
-function showError(){
-console.log("Error in writing to the database");
+function writePosition(x,y) {
+  database.ref('balloon/position').set({
+      'x' : position.x + x,
+      'y' : position.y + y,
+  })    
+
 }
